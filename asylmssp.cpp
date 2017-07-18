a4b519f8bba0b7b430813f03ae2fbe58b55668c0
ALH <asylhawa@rocketmail.com>
2017-07-18 17:41:08 +0100

    initial commit from laptop

---
+++ asylmssp.cpp
@@ -0,0 +1,41 @@
+/*--------------------/
+ALH
+Rewritten Minimum Score Separation Problem
+17/07/2017
+/--------------------*/
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <cstdio>
+#include <cstdlib>
+#include <vector>
+#include <time.h>
+#include <math.h>
+#include <float.h>
+#include <queue>
+#include <limits.h>
+#include <algorithm>
+#include <iomanip> //header providing parametric manipulators
+using namespace std;
+
+
+int main(){
+
+	time_t startTime, endTime;
+	startTime = clock();
+
+
+
+
+
+
+
+
+
+
+	endTime = clock();
+	int totalTime = (int)(((endTime - startTime) / double(CLOCKS_PER_SEC)) * 100);
+	cout << "CPU Time = " << totalTime << " milliseconds.\n";
+
+}
