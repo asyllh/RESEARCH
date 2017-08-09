d7be8a126f74a2f6a2db5efeaa2c82c69a1d00d8
ALH <asylhawa@rocketmail.com>
2017-08-09 13:22:28 +0100

    added createInstance function, sorted out variables, changed reset/clear vector functions

--- msspoutput.cpp
+++ msspoutput.cpp
@@ -11,27 +11,17 @@ MSSP with output of solution.
 #include <iomanip> //header providing parametric manipulators
 using namespace std;
 
-void resetVectors(int vacant, int numScores, int numComp,  vector<vector<int> > &adjMatrix, vector<int> &allScores, vector<int> &checked, vector<int> &cycleVertex, vector<int> &matchList, vector<int> &mates, vector<int> &QSet, vector<vector<int> > &S, vector<int> &patchCycle, vector<int> &patchVertex){
+void resetVectors(int vacant, int numScores, int numComp, vector<int> &allScores, vector<vector<int> > &adjMatrix, vector<int> &cycleVertex, vector<int> &matchList, vector<int> &mates, vector<vector<int> > &S){
 	int i, j;
 
 	for(i = 0; i < numScores; ++i){
 		allScores[i] = 0;
-		matchList[i] = vacant;
 		cycleVertex[i] = 1;
+        matchList[i] = vacant;
 		mates[i] = 0;
-		checked[i] = 0;
-        patchVertex[i] = vacant;
-	}
-
-	for(i = 0; i < numScores; ++i){
 		for(j = 0; j < numScores; ++j){
-			adjMatrix[i][j] = 0;
-		}
-	}
-
-	for(i = 0; i < numComp; ++i){
-		QSet[i] = 0;
-        patchCycle[i] = vacant;
+            adjMatrix[i][j] = 0;
+        }
 	}
 
 	for(i = 0; i < numComp; ++i){
@@ -42,19 +32,11 @@ void resetVectors(int vacant, int numScores, int numComp,  vector<vector<int> > 
 
 }
 
-void clearVectors(vector<int> &cycle, vector<int> &edge, vector<int> &lengthMateInduced, vector<vector<int> > &mateInduced, vector<int> &randOrder, vector<int> &t, vector<vector<int> > &T, vector<int> &SSet, vector<int> &fullCycle, vector<vector<int> > &Tpatch, vector<int> &temp){
-	cycle.clear();
-	edge.clear();
-	lengthMateInduced.clear();
-	mateInduced.clear();
-	randOrder.clear();
-	t.clear();
-	T.clear();
-	SSet.clear();
-    fullCycle.clear();
-    Tpatch.clear();
-    temp.clear();
+void clearVectors(vector<vector<int> > &mateInduced, vector<int> &lengthMateInduced, vector<vector<int> > &T){
 
+    mateInduced.clear();
+    lengthMateInduced.clear();
+    T.clear();
 }
 
 void loopCycle(int v, int full, int save, vector<int> &matchList, vector<int> &cycleVertex, vector<int> &fullCycle, vector<vector<int> > &mateInduced, vector<vector<int> > &T){
@@ -287,12 +269,67 @@ void loopCyclePatch(int &u, int v, int save, int vacant, vector<int> &matchList,
 
 }
 
+void createInstance(int threshold, int minWidth, int maxWidth, int numScores, int numBox, vector<int> &allScores, vector<vector<int> > &adjMatrix, vector<int> &mates){
+    int i, j;
+    vector<int> randOrder;
+
+    //Create random values to be used as score widths, put in allScores vector (except last two elements)
+    for (i = 0; i < numScores - 2; ++i) {
+        allScores[i] = rand() % (maxWidth - minWidth + 1) + minWidth;
+    }
+    //add two dominating vertices with score widths = 71 (these scores will be either side of same box, mates)
+    allScores[numScores - 2] = 70;
+    allScores[numScores - 1] = 70;
+
+    //Sort all of the scores in the allScores vector in ascending order
+    sort(allScores.begin(), allScores.end()); //sorts elements of vector in ascending order
+
+    //Filling in adjacency matrix - if sum of two scores >= threshold (70), then insert 1 into the matrix, else leave as 0
+    for (i = 0; i < allScores.size() - 1; ++i) {
+        for (j = i + 1; j < allScores.size(); ++j) {
+            if (allScores[i] + allScores[j] >= threshold) {
+                adjMatrix[i][j] = 1;
+                adjMatrix[j][i] = 1;
+            }
+        }
+
+    }
+
+    //Initially, randOrder vector will contain elements in the order 0, ..., numScores -2, numScores -1
+    for (i = 0; i < numScores; ++i) {
+        randOrder.push_back(i);
+    }
+
+    //Randomly shuffle all values in randOrder vector EXCEPT the last two values (dominating vertices, must stay as mates)
+    random_shuffle(randOrder.begin(), randOrder.begin() + (numScores - 2));
+
+    //Assign mates to each score (i.e. pair up scores to define which scores are either side of the same box)
+    //In the adjacency matrix, this will be represented by value 2
+    //Therefore there will be a value of 2 in every row and every column, non repeating
+    for (i = 0; i < numBox; ++i) {
+        adjMatrix[randOrder[2 * i]][randOrder[2 * i + 1]] = 2;
+        adjMatrix[randOrder[2 * i + 1]][randOrder[2 * i]] = 2;
+    }
+
+    for (i = 0; i < numScores; ++i) {
+        for (j = 0; j < numScores; ++j) {
+            if (adjMatrix[i][j] == 2) {
+                mates[i] = j;
+                break;
+            }
+        }
+    }
+    /*cout << "Mates Vector:\n";*/
+
+}
+
 void MTGMA(int vacant, int threshold, int numScores, int &matchSize, vector<int> &allScores, vector<vector<int> > &adjMatrix, vector<int> &cycleVertex, vector<int> &matchList){
 
 	int i, j, k;
 	int lastMatch = vacant;
 	int mateMatch = vacant;
 	int vacantFlag = 0;
+    matchSize = 0;
 
 	for (i = 0; i < numScores; ++i) { //check all vertices
 		vacantFlag = 0;
@@ -347,17 +384,8 @@ void MIS(int numScores, int &numCycles, vector<vector<int> > &adjMatrix, vector<
 	int currentVertex;
 	vector<int> cycle;
 	vector<int> checked(numScores, 0);
+    numCycles = 0;
 
-	for (i = 0; i < numScores; ++i) {
-		for (j = 0; j < numScores; ++j) {
-			if (adjMatrix[i][j] == 2) {
-				mates[i] = j;
-				break;
-			}
-		}
-	}
-
-	/*cout << "Mates Vector:\n";*/
 
 	//find the smallest vertex not yet checked for mate-induced structure - start with this vertex
 	for (i = 0; i < numScores; ++i) {
@@ -631,10 +659,6 @@ void patchGraph(int vacant, int instance, int numCycles, int &feasible, int &ful
 
 	}
 
-
-
-
-
 }
 
 
@@ -666,40 +690,26 @@ int main(int argc, char **argv){
 	int vacant = 999; //large empty value
 	int feasible = 0; //number of feasible instances
 	int infeasible = 0; //number of infeasible instances
+    int noMatch = 0; //number of instances with |M| < n (therefore immediately infeasible)
+    int oneCycle = 0; //number of instances where the MIS consists of only one cycle (therefore immediately feasible)
+    int noFam = 0; //number of instances with no family of T-cycles (qstar = -1, therefore immediately infeasible)
+    int noPatch = 0; //number of instances where T-cycles do not produce a connected patching graph (SSum < numCycles, therefore infeasible)
+    int fullT = 0; //number of instances where only one T-cycle is required to connected all cycles in MIS (patching graph is connected using only one T-cycle, therefore feasible)
+    int splitT = 0; //number of instances where multiple T-cycles are required to connected all cycles in MIS (patching graph is connected using multiple T-cycles, therefore feasible)
+    int problemInstance = 0; //number of problematic instances, SSum > numCycles (ERROR)
 
-	//Creating scores and forming adjacency matrix:
-	vector<int> allScores(numScores, 0); //vector containing all score widths
-	vector<int> randOrder; //vector used to shuffle and assign mates
-	vector<vector<int> > adjMatrix(numScores, vector<int>(numScores ,0)); //adjacency matrix, 0 if width sum < threshold, 1 if width sum >= threshold, 2 if scores are mates (either side of same box)
-
-	//Modified Threshold Graph Matching Algorithm (MTGMA)
-	int matchSize = 0; //size/cardinality of the matching list
-	int noMatch = 0; //counter for number of instances with |M| < n (matchSize < numBox)
-	vector<int> cycleVertex(numScores, 1); //contains the number of the cycle of the mate-induced structure that the vertex i belongs to
-	//(e.g. if vertex 4 is in the first cycle of the MIS, then cycleVertex[4] = 0 (0 = first cycle, 1 = second cycle etc))
-	vector<int> matchList(numScores, vacant); //contains vertex index for matching vertices, e.g. if vertex 0 is matched with vertex 9, then matchList[0] = 9
-
-	//Mate-Induced Structure (MIS)
-	int numCycles; //number of cycles in the mate-induced structure, i.e mateInduced.size() (each row in the mate-induced structure matrix represents a cycle)
-	int oneCycle = 0; //counter for the number of instances where the MIS consists of one one cycle (therefore immediately feasible)
-	vector<int> mates(numScores, 0); //contains vertex index for mates, e.g if vertex 0 is mates with vertex 4, then mates[0] = 4
-	vector<vector<int> > mateInduced; //size numscore by noComp, i.e. number of rows = numScores, number of columns = noComp
-	vector<int> lengthMateInduced; //each element holds the value corresponding to the length of the relative cycle in the mate-induced structure
-
-	//Familty Construction Algorithm (FCA)
-	int qstar; //number of T-cycle families found (i.e T.size())
-	int noFam = 0; //counter for number of instances where no family of T-cycles has been found
-	vector<vector<int> > T; //holds set of edges in T-cycles
-	vector<vector<int> > S(numComp, vector<int>(numComp, 0)); // set of indices of cycles of MIS that have an edge in T, == 1 if edge from cycle j is used in T-cycle q (T[q][j])
-	//FCA also uses the vector cycleVertex from MTGMA
-
-	//Patching Graph Connectivity
-	int problemInstance = 0; //counter for number of instances with issues (SSum > numCycles)
-	int noPatch = 0;
-
-	//Building solution output
-    int splitT = 0;
-    int fullT = 0;
+    int matchSize; //size (cardinality) of the matching list (matchList.size()) (&MTGMA, FCA)
+    int numCycles; //number of cycles in the MIS (mateInduced.size()) (&MIS, patchGraph)
+    int qstar; //number of T-cycles (&FCA, patchGraph)
+    vector<int> allScores(numScores, 0); //vector containing all score widths (createInstance, MTGMA)
+    vector<vector<int> > adjMatrix(numScores, vector<int>(numScores, 0)); //adjaceny matrix (createInstance, MTGMA, MIS, FCA)
+    vector<int> mates(numScores, 0); //contains vertex index for mates, e.g if vertex 0 is mates with vertex 4, then mates[0] = 4 (createInstance, MIS)
+    vector<int> matchList(numScores, vacant); //contains vertex index for matching vertices, e.g. if vertex 0 is matched with vertex 9, then matchList[0] = 9 (MTGMA, MIS, FCA, patchGraph)
+    vector<int> cycleVertex(numScores, 1); //contains the number of the cycle of the mate-induced structure that the vertex i belongs to (MTGMA, FCA, patchGraph)
+    vector<vector<int> > mateInduced; //each row of the matrix corresponds to one cycle, and contains the indices of the score widths (MIS, FCA, patchGraph)
+    vector<int> lengthMateInduced; //each elements holds the value of the length of the corresponding cycle in the MIS (MIS)
+    vector<vector<int> > S(numComp, vector<int>(numComp, 0)); // == 1 if edge from cycle j is used in T-cycle q (T[q][j]) (FCA, patchGraph)
+    vector<vector<int> > T; //each row hold the lower vertex of the edges that make up one T-cycle
 	srand(randomSeed); //seed
 
 	cout << "Minimum Score Separation Problem - Matching-Based Alternating Hamiltonicity Recognition Algorithm\n\n";
@@ -708,51 +718,14 @@ int main(int argc, char **argv){
 	startTime = clock();
 
 	for(instance = 0; instance < numInstances; ++instance) {
+        resetVectors(vacant, numScores, numComp, allScores, adjMatrix, cycleVertex, matchList, mates, S);
+        clearVectors(mateInduced, lengthMateInduced, T);
 
-		resetVectors(vacant, numScores, numComp, adjMatrix, allScores, checked, cycleVertex, matchList, mates, QSet, S, patchCycle, patchVertex);
-		clearVectors(cycle, edge, lengthMateInduced, mateInduced, randOrder, t, T, SSet, fullCycle, Tpatch, temp);
 
-		//Create random values to be used as score widths, put in allScores vector (except last two elements)
-		for (i = 0; i < numScores - 2; ++i) {
-			allScores[i] = rand() % (maxWidth - minWidth + 1) + minWidth;
-		}
-		//add two dominating vertices with score widths = 71 (these scores will be either side of same box, mates)
-		allScores[numScores - 2] = 70;
-		allScores[numScores - 1] = 70;
-
-		//Sort all of the scores in the allScores vector in ascending order
-		sort(allScores.begin(), allScores.end()); //sorts elements of vector in ascending order
-
-		//Filling in adjacency matrix - if sum of two scores >= threshold (70), then insert 1 into the matrix, else leave as 0
-		for (i = 0; i < allScores.size() - 1; ++i) {
-			for (j = i + 1; j < allScores.size(); ++j) {
-				if (allScores[i] + allScores[j] >= threshold) {
-					adjMatrix[i][j] = 1;
-					adjMatrix[j][i] = 1;
-				}
-			}
-
-		}
-
-		//Initially, randOrder vector will contain elements in the order 0, ..., numScores -2, numScores -1
-		for (i = 0; i < numScores; ++i) {
-			randOrder.push_back(i);
-		}
-
-		//Randomly shuffle all values in randOrder vector EXCEPT the last two values (dominating vertices, must stay as mates)
-		random_shuffle(randOrder.begin(), randOrder.begin() + (numScores - 2));
-
-		//Assign mates to each score (i.e. pair up scores to define which scores are either side of the same box)
-		//In the adjacency matrix, this will be represented by value 2
-		//Therefore there will be a value of 2 in every row and every column, non repeating
-		for (i = 0; i < numBox; ++i) {
-			adjMatrix[randOrder[2 * i]][randOrder[2 * i + 1]] = 2;
-			adjMatrix[randOrder[2 * i + 1]][randOrder[2 * i]] = 2;
-		}
+        createInstance(threshold, minWidth, maxWidth, numScores, numBox, allScores, adjMatrix, mates);
 
 		//MATCHING ALGORITHM MTGMA
 		MTGMA(vacant, threshold, numScores, matchSize, allScores, adjMatrix, cycleVertex, matchList);
-
 
 		/*cout << "Cycle Vertex vector after MTGMA:\n";*/
         /*cout << "Matching List:\n";*/
@@ -767,7 +740,6 @@ int main(int argc, char **argv){
 		}
 
 		MIS(numScores, numCycles, adjMatrix, mates, matchList, mateInduced, lengthMateInduced);
-
 
 		//If the mate-induced structure only consists of one cycle, then the problem has been solved and is feasible (just remove one matching edge to find feasible path)
 		if (lengthMateInduced[0] == numScores) { //if all of the vertices are in the first (and only) cycle of the mate-induced structure
@@ -792,7 +764,6 @@ int main(int argc, char **argv){
 			++noFam;
 			continue;
 		}
-
 
 		//CHECK IF PATCHING GRAPH IS CONNECTED
 		patchGraph(vacant, instance, numCycles, feasible, fullT, qstar, numScores, splitT, noPatch, infeasible, problemInstance, matchList, cycleVertex, mateInduced, T, S);
