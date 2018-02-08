730cb35bfe95d2a46151be840ff518e81afdcfe1
ALH <asylhawa@rocketmail.com>
2018-02-08 11:05:48 +0000

    added two new functions

--- paper/SCSPP/packing.h
+++ paper/SCSPP/packing.h
@@ -14,7 +14,13 @@ void swap(int &a, int &b);
 
 int lowerBound(int maxStripWidth, double totalBoxWidth);
 
-void packStripsFFD(int numScores, int numBox, int maxBoxWidth, int maxStripWidth, double totalBoxWidth, vector<int> &allScores, vector<int> &mates,
+void packStripsFFDApprox(int numScores, int numBox, int maxBoxWidth, int maxStripWidth, double totalBoxWidth, vector<int> &allScores, vector<int> &mates,
+                         vector<vector<int> > &adjMatrix, vector<vector<int> > &boxWidths, vector<int> &stripSum, vector<vector<int> > &strip);
+
+void packStripsFFDSmallest(int numScores, int numBox, int maxBoxWidth, int maxStripWidth, double totalBoxWidth, vector<int> &allScores, vector<int> &mates,
+                         vector<vector<int> > &adjMatrix, vector<vector<int> > &boxWidths, vector<int> &stripSum, vector<vector<int> > &strip);
+
+void packStripsFFDExact(int numScores, int numBox, int maxBoxWidth, int maxStripWidth, double totalBoxWidth, vector<int> &allScores, vector<int> &mates,
                    vector<vector<int> > &adjMatrix, vector<vector<int> > &boxWidths, vector<int> &stripSum, vector<vector<int> > &strip);
 
 void MBAHRA(int i1, int j1, int &feasible, vector<int> &allScores, vector<int> &mates, vector<vector<int> > &adjMatrix,
