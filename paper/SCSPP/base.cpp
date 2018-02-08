b94e06a5dd1896e0a3e725abe1a6f68b7fb5e5a0
ALH <asylhawa@rocketmail.com>
2018-02-08 13:46:02 +0000

    output of allscores

--- paper/SCSPP/base.cpp
+++ paper/SCSPP/base.cpp
@@ -62,10 +62,10 @@ void createInstance(int numScores, int numBox, int minWidth, int maxWidth, int m
     sort(allScores.begin(), allScores.end()); //sorts elements of vector in ascending order
 
     //cout << "All scores:\n";
-    /*for(i = 0; i < allScores.size(); ++i){
+    for(i = 0; i < allScores.size(); ++i){
         cout << allScores[i] << " ";
     }
-    cout << endl;*/
+    cout << endl;
     //Filling in adjacency matrix - if sum of two scores >= threshold (70), then insert 1 into the matrix, else leave as 0
     for (i = 0; i < allScores.size() - 1; ++i) {
         for (j = i + 1; j < allScores.size(); ++j) {
