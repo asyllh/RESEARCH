c2a4b56f3f555d7d8bd83df23838f258de4d1fec
ALH <asylhawa@rocketmail.com>
2018-03-07 22:31:07 +0000

    added void programInfo and void argumentCheck, changed input method for userarguments, added parameters to calling functions EA, createInstance and createInitialPopulation

--- EA/main.cpp
+++ EA/main.cpp
@@ -3,82 +3,160 @@ ALH
 main.cpp
 Evolutionary Algorithm with Local Search
 05/12/2017
+06/03/2018
 /--------------*/
 #include <iostream>
 #include <fstream>
 #include <ctime>
+#include <cstring>
+#include <iomanip>
 using namespace std;
 
 #include "base.h"
 #include "packing.h"
 
-int main(int argc, char **argv){
-    //region USAGE - ARGUMENTS REQUIRED
-    if(argc < 8){
-        cout << "Minimum Score Separation Problem: AHCA.\n";
-        cout << "Arguments are the following:\n";
-        cout << "- Number of instances (integer)\n";
-        cout << "- Number of items (integer)\n";
-        cout << "- Minimum width of scores (millimeters, min = 1)\n";
-        cout << "- Maximum width of scores (millimeters, max = 70)\n";
-        cout << "- Minimum width of boxes (millimeters, min = 140)\n";
-        cout << "- Maximum width of boxes (millimeters, max = 1000)\n";
-        cout << "- Maximum width of strips (millimeters)\n";
-        cout << "- Random Seed (integer)\n";
-        cout << "- Name of input file (must have .txt extension)\n";
+void programInfo(){
+
+    cout << "Evolutionary Algorithm for the SCSPP:\n-------------\n"
+         << "PARAMETERS:\n"
+         << "       -i <int>    [Number of instances. Default = 1000.]\n"
+         << "       -t <int>    [Constraint value. Default = 70.]\n"
+         << "       -n <int>    [Number of items. Default = 500.]\n"
+         << "       -a <int>    [Minimum score width. Default = 1.]\n"
+         << "       -b <int>    [Maximum score width. Default = 70.]\n"
+         << "       -w <int>    [Minimum item width. Default = 150.]\n"
+         << "       -W <int>    [Maximum item width. Default = 1000.]\n"
+         << "       -l <int>    [Length of strips. Default = 5000.]\n"
+         << "       -s <int>    [Random seed. Default = 1.]\n"
+         << "---------------\n\n";
+}
+
+void argumentCheck(int numInstances, int tau, int numItem, int minWidth, int maxWidth, int minItemWidth, int maxItemWidth,
+                   int maxStripWidth, int randomSeed){
+    cout << "Evolutionary Algorithm for the SCSPP\n------------------------------\n";
+    if(tau == 0){
+        cout << "[ERROR]: Constraint value cannot be zero.\n";
         exit(1);
+    }
+    if(maxStripWidth == 0){
+        cout << "[ERROR]: Strip cannot have length zero.\n";
+        exit(1);
+    }
+    if(2*minWidth >= tau){
+        cout << "[ERROR]: Constraint value is less than or equal to twice the minimum score width, vicinal sum constraint always valid.\n";
+        cout << "         Problem instance is therefore classical strip-packing problem without score constraint (i.e. tau = 0).\n";
+        exit(1);
+    }
+    if(2*maxWidth < tau){
+        cout << "[ERROR]: Constraint value is greater than double maximum score width, vicinal sum constraint never valid.\n";
+        exit(1);
+    }
+    if(2*maxWidth >= minItemWidth){
+        cout << "[ERROR]: Minimum item width is less than double maximum score width, scores may overlap.\n";
+        exit(1);
+    }
+    if(minWidth > maxWidth){
+        cout << "[ERROR]: Minimum score width is greater than maximum score width.\n";
+        exit(1);
+    }
+    if(maxItemWidth > maxStripWidth){
+        cout << "[ERROR]: Maximum item width is larger than length of strip.\n";
+        exit(1);
+    }
+
+    cout << std::left << setw(20) << "Number of instances:" << std::right << setw(10) << numInstances << endl
+         << std::left << setw(20) << "Constraint value:" << std::right << setw(10) << tau << endl
+         << std::left << setw(20) << "Number of items:" << std::right << setw(10) << numItem << endl
+         << std::left << setw(20) << "Minimum score width:" << std::right << setw(10) << minWidth << endl
+         << std::left << setw(20) << "Maximum score width:" << std::right << setw(10) << maxWidth << endl
+         << std::left << setw(20) << "Minimum item width:" << std::right << setw(10) << minItemWidth << endl
+         << std::left << setw(20) << "Maxmimum item width:" << std::right << setw(10) << maxItemWidth << endl
+         << std::left << setw(20) << "Length of strips:" << std::right << setw(10) << maxStripWidth << endl
+         << std::left << setw(20) << "Random seed:" << std::right << setw(10) << randomSeed << endl;
+    cout << "------------------------------\n\n";
+}
+
+int main(int argc, char **argv){
+    if(argc <= 1){
+       programInfo();
+        exit(1);
+    }
+
+    int x;
+    int numInstances = 1000;
+    int tau = 70;
+    int numItem = 500;
+    int minWidth = 1;
+    int maxWidth = 70;
+    int minItemWidth = 150;
+    int maxItemWidth = 1000;
+    int maxStripWidth = 5000;
+    int randomSeed = 1;
+
+    //region User Arguments
+    for(x = 1; x < argc; ++x){
+        if(strcmp("-i", argv[x]) == 0){
+            numInstances = atoi(argv[++x]);
+        }
+        else if(strcmp("-t", argv[x]) == 0){
+            tau = atoi(argv[++x]);
+        }
+        else if(strcmp("-n", argv[x]) == 0){
+            numItem = atoi(argv[++x]);
+        }
+        else if(strcmp("-a", argv[x]) == 0){
+            minWidth = atoi(argv[++x]);
+        }
+        else if(strcmp("-b", argv[x]) == 0){
+            maxWidth = atoi(argv[++x]);
+        }
+        else if(strcmp("-w", argv[x]) == 0){
+            minItemWidth = atoi(argv[++x]);
+        }
+        else if(strcmp("-W", argv[x]) == 0){
+            maxItemWidth = atoi(argv[++x]);
+        }
+        else if(strcmp("-l", argv[x]) == 0){
+            maxStripWidth = atoi(argv[++x]);
+        }
+        else if(strcmp("-s", argv[x]) == 0){
+            randomSeed = atoi(argv[++x]);
+        }
     }
     //endregion
 
-    //region VARIABLES
-    //int numInstances = atoi(argv[1]); //number of instances of mssp, use in main for loop
-    int numItem = atoi(argv[1]); //number of boxes in mssp plus 1 extra box (scores on either side of extra box will be dominating vertices, score widths = 71)
-    int minWidth = atoi(argv[2]); //minimum width of scores (millimeters)
-    int maxWidth = atoi(argv[3]); //maximum width of scores (millimeters)
-    int minItemWidth = atoi(argv[4]); //min box width (mm)
-    int maxItemWidth = atoi(argv[5]); //max box width (mm)
-    int maxStripWidth = atoi(argv[6]);
-    int randomSeed = atoi(argv[7]); //random seed
+    argumentCheck(numInstances, tau, numItem, minWidth, maxWidth, minItemWidth, maxItemWidth, maxStripWidth, randomSeed);
 
-    int i, j, q, n;
-    int instance; //counter for instances loop
-    int numScores = numItem * 2; //number of scores, 2 per box (1 either side), last two scores are dominating vertices
-    //int threshold = 70; //adjacency threshold of scores, minimum knife distance
+    int i;
+    int instance;
+    int numScores = numItem * 2;
+    double totalItemWidth;
     vector<int> allScores;
-    vector<vector<int> > adjMatrix(numScores, vector<int>(numScores, 0)); //adjaceny matrix (createInstance, MTGMA, MIS, FCA)
-    vector<int> partners(numScores, 0); //contains vertex index for partners, e.g if vertex 0 is partners with vertex 4, then partners[0] = 4 (createInstance, MIS)
-    vector<vector<int> > itemWidths(numScores, vector<int>(numScores, 0)); // holds widths in mm for the widths of boxes
-    vector<vector<int> > allItems(numScores, vector<int>(numScores, 0)); //contains values from 1 to numItem to denote which box the scores belong to
-    double totalItemWidth = 0.0;
-    //vector<int> stripSum(numItem, 0);
-    //vector<vector<int> > strip(numItem);
+    vector<int> partners(numScores, 0);
+    vector<vector<int> > itemWidths(numScores, vector<int>(numScores, 0));
+    vector<vector<int> > adjMatrix(numScores, vector<int>(numScores, 0));
+    vector<vector<int> > allItems(numScores, vector<int>(numScores, 0));
     vector<vector<vector<int> > > population;
     vector<vector<int> > populationSum;
     double parent1cost;
     double parent2cost;
-    //endregion
-
-
-
-    cout << "local search\n-------------\n";
-
-    //srand(unsigned(time(NULL))); //seed
     srand(randomSeed);
 
 
     time_t startTime, endTime; //start clock
     startTime = clock();
 
-    createInstance(numScores, numItem, minWidth, maxWidth, minItemWidth, maxItemWidth, totalItemWidth, allScores, partners, adjMatrix, itemWidths, allItems);
+    createInstance(tau, numScores, numItem, minWidth, maxWidth, minItemWidth, maxItemWidth, totalItemWidth, allScores, partners, adjMatrix, itemWidths, allItems);
 
-    createInitialPopulation(numScores, numItem, maxItemWidth, maxStripWidth, allScores, partners, adjMatrix, itemWidths, populationSum, population);
+    createInitialPopulation(tau, numScores, numItem, maxItemWidth, maxStripWidth, allScores, partners, adjMatrix, itemWidths, populationSum, population);
 
-    EA(numScores, maxItemWidth, maxStripWidth, parent1cost, parent2cost, allScores, partners, adjMatrix, itemWidths, populationSum, population);
+    EA(tau, numScores, maxItemWidth, maxStripWidth, parent1cost, parent2cost, allScores, partners, adjMatrix, itemWidths, populationSum, population);
 
 
     endTime = clock();
-    double totalTime = (((endTime - startTime) / double(CLOCKS_PER_SEC)) * 1000);
-    cout << "\nCPU Time = " << totalTime << " milliseconds.\nEND.\n";
+    double totalTimeMS = (((endTime - startTime) / double(CLOCKS_PER_SEC)) * 1000);
+    double totalTimeS = totalTimeMS / 1000;
+    cout << "\nCPU Time = " << totalTimeMS << " milliseconds (" << totalTimeS << " seconds.)\nEND.\n";
 
 
 }//END INT MAIN
